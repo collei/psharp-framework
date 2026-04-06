e3b72044f165fdcd180dd2f41f735dd02713cf48
Collei <collei@collei.com.br>
2026-04-05 21:23:52 -0300

    added docblocks

--- src/PSharp/Core/DI/ContainerException.php
+++ src/PSharp/Core/DI/ContainerException.php
@@ -4,6 +4,9 @@ namespace PSharp\Core\DI;
 use Exception;
 use Psr\Container\ContainerExceptionInterface;
 
+/**
+ * Exceptions thrown by the Container
+ */
 class ContainerException extends Exception implements ContainerExceptionInterface
 {
     //
--- src/PSharp/Core/DI/NotFoundException.php
+++ src/PSharp/Core/DI/NotFoundException.php
@@ -4,6 +4,9 @@ namespace PSharp\Core\DI;
 use Exception;
 use Psr\Container\NotFoundExceptionInterface;
 
+/**
+ * Exception thrown when class not found.
+ */
 class NotFoundException extends ContainerException implements NotFoundExceptionInterface
 {
     //
