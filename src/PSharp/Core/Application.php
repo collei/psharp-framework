f20c0606c7cc1a77354a3a73d159bf384cfa8051
Collei <collei@collei.com.br>
2026-04-10 20:51:53 -0300

    added docblocks

--- src/PSharp/Core/Application.php
+++ src/PSharp/Core/Application.php
@@ -4,19 +4,53 @@ namespace PSharp\Core;
 use Closure;
 use PSharp\Support\Pipeline;
 use PSharp\Support\Str;
-use PSharp\Http\{RouteMapper,Router,Request,Response};
+use PSharp\Http\{RouteMapper, Router, Request, Response};
 use PSharp\Http\Factories\RequestFactory;
 use PSharp\Http\Actions\ControllerBase;
 
+/**
+ * The application main class.
+ */
 final class Application
 {
+    /**
+     * @var string
+     */
     private $baseDir;
+
+    /**
+     * @var PSharp\Core\Container
+     */
     private $container;
+
+    /**
+     * @var PSharp\Core\Config
+     */
     private $config;
+
+    /**
+     * @var PSharp\Http\RouteMapper
+     */
     private $routeMapper;
+
+    /**
+     * @var PSharp\Http\Router
+     */
     private $router;
+
+    /**
+     * @var array
+     */
     private $middleware = [];
 
+    /**
+     * Initializes application.
+     * 
+     * @param string $baseDir - must be equivalent to the __DIR__ magic constant
+     *                          in the calling file located at the very app root folder.
+     * @param string|null $configFile - a JSON file name, relatively located to the app root folder.
+     *                          Default is 'appsettings.json' 
+     */
     public function __construct(string $baseDir, string $configFile = null)
     {
         $this->baseDir = $baseDir;
@@ -25,6 +59,12 @@ final class Application
         $this->initialize();
     }
 
+    /**
+     * Retrieves properties.
+     * 
+     * @property PSharp\Core\Container container
+     * @property PSharp\Http\Router router
+     */
     public function __get(string $name)
     {
         if (in_array($name, ['container','router'])) {
@@ -34,6 +74,11 @@ final class Application
         return null;
     }
 
+    /**
+     * Initializes the internal instances.
+     * 
+     * @return void
+     */
     protected function initialize()
     {
         $this->container = new Container();
@@ -44,6 +89,12 @@ final class Application
         $this->router = $this->container->make(Router::class);
     }
 
+    /**
+     * Loads the configuration file from the disk.
+     * 
+     * @param string $configFile
+     * @return PSharp\Core\Config
+     */
     protected function loadConfig(string $configFile)
     {
         $configFile = $this->path($configFile);
@@ -51,16 +102,34 @@ final class Application
         return new Config($configFile);
     }
 
+    /**
+     * Retrieves the container instance.
+     * 
+     * @return PSharp\Core\Container
+     */
     public function container()
     {
         return $this->container;
     }
 
+    /**
+     * Retrieves the router instance.
+     * 
+     * @return PSharp\Http\Router
+     */
     public function router()
     {
         return $this->router;
     }
 
+    /**
+     * Crafts a file or directory path relatively to the app rot folder, using segments.
+     * 
+     * Similar to C# Path.Combine() method, internally uses the PHP DIRECTORY_SEPARATOR constant.
+     * 
+     * @param string ...$segments
+     * @return string
+     */
     public function path(string ...$segments)
     {
         $further = empty($segments) ? null : implode(DIRECTORY_SEPARATOR, $segments);
@@ -72,11 +141,25 @@ final class Application
         return $this->baseDir;
     }
 
+    /**
+     * Return a valeu or a set of values from the config.
+     * 
+     * @param string $name - use dotted name for single values or smaller portions.
+     * @param mixed $default - use to give a default if value not found on config.
+     * @return string|array
+     */
     public function config(string $name, $default = null)
     {
         return $this->config->get($name, $default);
     }
 
+    /**
+     * Maps all declared endpoints from the given controller
+     * class or instance, if any.
+     * 
+     * @param string|PSharp\Http\Actions\ControllerBase|null $controller
+     * @return $this
+     */
     public function mapController(string|ControllerBase $namespace = null)
     {
         $this->routeMapper->mapController($controller);
@@ -84,6 +167,13 @@ final class Application
         return $this;
     }
 
+    /**
+     * Maps all declared endpoints from every controller declared under
+     * the given namespace, if any.
+     * 
+     * @param string|null $namespace
+     * @return $this
+     */
     public function mapControllers(string $namespace = null)
     {
         $this->routeMapper->mapControllers($this->baseDir, $namespace);
@@ -91,6 +181,12 @@ final class Application
         return $this;
     }
 
+    /**
+     * Adds middleware to the application middleware stack.
+     * 
+     * @param stirng ...$middleware
+     * @return $this
+     */
     public function useMiddleware(string ...$middleware)
     {
         foreach ($middleware as $one) {
@@ -104,6 +200,12 @@ final class Application
         return $this;
     }
 
+    /**
+     * Adds a Closure as middleware to the application middleware stack.
+     * 
+     * @param Closure $middleware
+     * @return $this
+     */
     public function use(Closure $middleware)
     {
         $this->middleware[] = $middleware;
@@ -111,6 +213,11 @@ final class Application
         return $this;
     }
 
+    /**
+     * Run the application by processing the request and serving the response.
+     * 
+     * @return @this
+     */
     public function run()
     {
         $request = $this->captureRequest();
@@ -120,6 +227,11 @@ final class Application
         echo $response;
     }
 
+    /**
+     * Executes the request capture process.
+     * 
+     * @return PSharp\Http\Request
+     */
     protected function captureRequest()
     {
         // conforma a REQUEST_URI à raiz da aplicação 
@@ -131,6 +243,12 @@ final class Application
         );
     }
 
+    /**
+     * Handles the request and returns the response.
+     * 
+     * @param PSharp\Http\Request $request
+     * @return mixed
+     */
     protected function handleRequest(Request $request)
     {
         try {
@@ -143,6 +261,12 @@ final class Application
         return '<div><b>algum erro ocorreu</b></div>';
     }
 
+    /**
+     * Sends forth the request through the middleware stack.
+     * 
+     * @param PSharp\Http\Request $request
+     * @return mixed
+     */
     protected function sendThroughRouter(Request $request)
     {
         return (new Pipeline())->send($request)
@@ -150,6 +274,12 @@ final class Application
                     ->then($this->dispatchToRouter($request));
     }
 
+    /**
+     * Sends forth the request to the router.
+     * 
+     * @param PSharp\Http\Request
+     * @return Closure
+     */
     protected function dispatchToRouter()
     {
         return function (Request $request) {
@@ -158,5 +288,4 @@ final class Application
             return $this->router->dispatch($request);
         };
     }
-
 }
