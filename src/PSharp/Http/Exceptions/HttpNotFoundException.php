4a3444ddf9326d1ca65bf71940a82f347a1d59b9
Collei <collei@collei.com.br>
2026-04-16 21:42:18 -0300

    removed unnecessary properties since they are inherited

--- src/PSharp/Http/Exceptions/HttpNotFoundException.php
+++ src/PSharp/Http/Exceptions/HttpNotFoundException.php
@@ -3,9 +3,6 @@ namespace PSharp\Http\Exceptions;
 
 class HttpNotFoundException extends HttpException
 {
-    protected $statusCode;
-    protected $headers;
-
     public function __construct(string $message = '', Throwable $previous = null, array $headers = null)
     {
         parent::__construct(404, $message, $previous, $headers);
