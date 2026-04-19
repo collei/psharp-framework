1f2eef5c2d51d6bc710b66c4089c6b17f3b681f0
Collei <collei@collei.com.br>
2026-04-19 20:32:17 -0300

    added path() function, changed get_instance_id() to return the first parameter when the second is set to true

--- src/helpers.php
+++ src/helpers.php
@@ -3,14 +3,32 @@
 /**
  * Returns the object instance id in the format (namesplaced_class_name@id).
  * 
- * @param object|null $object
- * @return string|null
+ * @param mixed $object
+ * @param bool $returnItIfNotObject = false - Set it to true to return the first parameter unaltered.
+ * @return mixed
  */
-function get_instance_id(object $object = null)
+function get_instance_id($object = null, bool $returnItIfNotObject = false)
 {
-    if ($object) {
+    if (is_object($object)) {
         return get_class($object) . '@' . spl_object_id($object);    
+    }
+
+    if ($returnItIfNotObject) {
+        return $object;
     }
 
     return null;
 }
+
+/**
+ * Returns a path to a file or directory, relative to the app basepath.
+ * 
+ * @param string ...$segments
+ * @return string
+ */
+function path(string ...$segments)
+{
+    return PSharp\Core\Application::getInstance()->path(...$segments);
+}
+
+/**EOF**/
