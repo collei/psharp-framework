5c02289502c188d6b69859bc2d5e257f2974dce8
Collei <collei@collei.com.br>
2026-04-20 14:17:31 -0300

    moved helper and facade files and made adjustements

--- src/PSharp/facades.php
+++ src/PSharp/facades.php
@@ -6,7 +6,7 @@ namespace {
      * 
      * Just accepts .php files - no other file will be loaded.
      */
-    $facadesPath = __DIR__ . DIRECTORY_SEPARATOR . 'facades' . DIRECTORY_SEPARATOR . '*.php';
+    $facadesPath = dirname(__DIR__) . DIRECTORY_SEPARATOR . '*.php';
 
     if ($files = glob($facadesPath)) foreach ($files as $file) {
         require $file;
