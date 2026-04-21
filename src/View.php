505827520583845aeadd792599e984afdd7e1e73
Collei <collei@collei.com.br>
2026-04-21 08:02:33 -0300

    added facade View

---
+++ src/View.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Facade class.
+ */
+
+/**
+ * Database facade.
+ */
+class View extends PSharp\Support\Facade
+{
+    /**
+     * Retrieves the available methods.
+     * 
+     * @return array
+     */
+    protected static function available()
+    {
+        return [
+            'file',
+            'make',
+            'share',
+            'shared',
+            'getShared',
+        ];
+    }
+
+    /**
+     * Retrieves the facade acessor.
+     * 
+     * @return string
+     */
+    protected static function getAcessor()
+    {
+        return PSharp\View\Factory::class;
+    }
+}
