422d4615de7fc7b382ef87d2769339a8a665b306
Collei <collei@collei.com.br>
2026-05-23 22:38:40 -0300

    added dump functions pretty_dump() and pretty_dd()

--- src/PSharp/helpers.php
+++ src/PSharp/helpers.php
@@ -128,4 +128,140 @@ if (! function_exists('db')) {
     }
 }
 
+if (! function_exists('pretty_dump')) {
+    /**
+     * Dumps variable and formats dump content in a browseable manner
+     * with toggleable levels.
+     * 
+     * @param mixed $value
+     * @param bool $return_value = false
+     * @param bool $open = false
+     * @return string|never
+     */
+    function pretty_dump($value, bool $return_value = false, bool $open = false)
+    {
+        // unique dump ID
+        static $dump_id = 0;
+
+        // built-in necessary CSS code and some vanilla Javascript
+        static $cssJscript = "
+            <style>
+            button.dumper-btn-toggle { height: 19px !important; border-top: 3px !important; }
+            button.dumper-btn-toggle[data-state=\"open\"]::before { content: '▲' }
+            button.dumper-btn-toggle[data-state=\"open\"] + span + div.dumper-panel>div { display: block; }
+            button.dumper-btn-toggle[data-state=\"closed\"]::before { content: '▼' }
+            button.dumper-btn-toggle[data-state=\"closed\"] + span + div.dumper-panel>div { display: none; }
+            span { color: #3ff !important; font-family: monospace; }
+            div.dumper-main { background-color: #020 !important; line-height: 1.75em !important; }
+            div.dumper-panel { background-color: #020 !important; padding: 0px 2px 0px 32px !important; }
+            div.dumper-panel>div { color: #ff3 !important; white-space: pre; font-family: monospace; line-height: 1.75em !important; }
+            </style>
+            <script>
+            function tggl(btnRef) { btnRef.setAttribute('data-state', ((btnRef.getAttribute('data-state') == 'open') ? 'closed' : 'open')); }
+            </script>
+        ";
+
+        $divState = $open ? 'open' : 'closed';
+
+        $lines = explode("\n", print_r($value, true));
+
+        list($levels, $level_prior, $element_count) = array([], 0, 1);
+
+        foreach ($lines as $k => $line) {
+            if ($line !== ltrim($line)) {
+                $line = str_repeat(' ', 4) . $line;
+            }
+
+            $line_cleaned = trim($line);
+
+            if (empty($line_cleaned)) {
+                unset($lines[$k]);
+                continue;
+            }
+
+            if ($line_cleaned === '(' || $line_cleaned === ')') {
+                unset($lines[$k]);
+                continue;
+            }
+
+            // crafts level based on indentation (8 spaces per level)
+            $level = (strlen($line) - strlen(ltrim($line))) / 8;
+
+            $line_ready = ($line_cleaned !== $line) ? ltrim($line) : $line;
+
+            if ($level < $level_prior) {
+                // close any opened DIVs
+                for ($co = $level_prior; $co > $level; --$co) {
+                    $levels[] = '</div></div>';
+                }
+            } elseif ($level > $level_prior) {
+                $last = count($levels) - 1;
+
+                // define div state
+                $state = ($level > 1) ? $divState : 'open';
+
+                // HTML elements
+                $btnName = sprintf('%sp%s', $dump_id, $element_count);
+                $button = "<button id=\"btnDump{$btnName}\" class=\"dumper-btn-toggle\" data-state=\"{$state}\" onclick=\"tggl(this)\"></button>";
+                $span = "<span>{$levels[$last]}</span>";
+                $div = "<div class=\"dumper-panel dumper-level-{$level}\"><div id=\"panelDump{$btnName}\">";
+
+                // append HTML elements
+                $levels[$last] = $button.$span.$div;
+
+                // update element count
+                ++$element_count;
+            }
+
+            $levels[] = trim($line_ready);
+
+            $level_prior = $level;
+        }
+
+        // closes any previous opened level DIVs
+        for ($co = $level_prior; $co > 0; --$co) {
+            $levels[] = '</div></div>';
+        }
+
+        // Only includes CSS and Javascript code on output
+        // when calling the dump() function at first time.
+        $dump_result = (0 === $dump_id) ? $cssJscript : '';
+
+        // Remove unnecessary line breaks from undesired places,
+        // so we can let css3 do the hard work accordingly.
+        $dump_result .= "<div class=\"dumper-main\" data-id=\"{$dump_id}\"><span></span>"
+            . str_replace(
+                [">\x01\x03\x02\x04[", "\x01\x03\x02\x04</", ">\x01\x03\x02\x04<", "\x01\x03\x02\x04"],
+                ['>[', '</', '><', "\n"],
+                implode("\x01\x03\x02\x04", $levels)
+            ) . '</div>';
+
+        // allows counting function calls
+        ++$dump_id;
+
+        if ($return_value) {
+            return $dump_result;
+        }
+
+        echo $dump_result;
+    }
+}
+
+if (! function_exists('pretty_dd')) {
+    /**
+     * Dumps variable and formats dump content in a browseable manner
+     * with toggleable levels and, then, stops script execution.
+     * 
+     * @param mixed $value
+     * @param bool $open = false
+     * @return never
+     */
+    function pretty_dd($value, bool $open = false)
+    {
+        pretty_dump($value, false, $open);
+
+        exit(0);
+    }
+}
+
 /**EOF**/
